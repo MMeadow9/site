ef6ec157be1ab3f97493abdc2a4979a44ad6ec08
Matvey <BarMatVla@gmail.com>
2024-03-24 15:06:20 +0300

    Message2

--- server.py
+++ server.py
@@ -3,9 +3,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
     return render_template("main.html")
 
 
-app.run(debug=True)
+app.run(port=8880, host='127.0.0.1')
